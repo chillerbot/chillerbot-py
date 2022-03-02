5363dc9d331daadfcb98a4fd8d4ff8c3b1507569
ChillerDragon <ChillerDragon@gmail.com>
2022-03-02 12:14:30 +0100

    Tbh I have no idea what I am doing

--- main.py
+++ main.py
@@ -7,5 +7,20 @@ SERVER_PORT = 8303
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-sock.sendto(b"foo", (SERVER_IP, SERVER_PORT))
+class CNetPacketConstruct():
+    def __init__(self):
+        self.token = 0
+        self.response_token = 0
+        self.flags = 0
+        self.ack = 0
+        self.num_chunks = 0
+        self.data_size = 0
+        self.chunk_data = b"foo"
 
+def SendPacket(packet: CNetPacketConstruct):
+    buf = packet.chunk_data
+    sock.sendto(buf, (SERVER_IP, SERVER_PORT))
+
+
+pck = CNetPacketConstruct()
+SendPacket(pck)
